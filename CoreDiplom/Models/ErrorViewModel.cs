e4cc44b9951c6098b7cea3eee040e43572561751
MaxKrasilnik <maxkrasilnik@gmail.com>
2020-05-14 19:34:11 +0300

    Добавьте файлы проекта.

---
+++ CoreDiplom/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreDiplom.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
